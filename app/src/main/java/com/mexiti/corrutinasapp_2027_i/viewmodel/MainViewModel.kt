a4c3316b9eed8532d20c5de6e338c741a3250e06
faraday58 <jarodriguezvera@gmail.com>
2026-09-19 20:21:34 -0600

    Solamente presenta envío de respuesta sin bloqueo de la iu principal

--- app/src/main/java/com/mexiti/corrutinasapp_2027_i/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/mexiti/corrutinasapp_2027_i/viewmodel/MainViewModel.kt
@@ -14,10 +14,7 @@ class MainViewModel:ViewModel(){
     var resultState by mutableStateOf("")
         private set
 
-    var countTime by mutableStateOf(0)
-        private set
-    var isLoading by mutableStateOf(false)
-        private set
+
 
 
     fun fetchData(){
@@ -28,38 +25,7 @@ class MainViewModel:ViewModel(){
         }
     }
 
-    fun fetchDataSecuencial(){
-        viewModelScope.launch {
-            resetState()
-            isLoading = true
-            resultState = "Consultadon 2 APIs en paralelo ..."
 
-            //Lanzamos dos tareas concurrentes
-            val job1 = async {
-                for ( i in 1..5){
-                    delay(1000)
-                    countTime = 1
-                }
-                "Datos de Usuario Ok"
-            }
-            val job2 = async {
-                delay(3000)
-                "Configuración Ok"
-            }
-            //Esperar el resultado de ambas tareas
-            val res1 = job1.await()
-            val res2 = job2.await()
-
-            resultState = "Sincronizando $res1  y $res2"
-            isLoading  = false
-
-        }
-    }
-
-    private fun resetState(){
-        countTime = 0
-        resultState = ""
-    }
 
  /*   fun bloqueoApp(){
         Thread.sleep(5000)
